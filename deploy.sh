8f18fd46f6aafe4dfa40eeb4215d7a4b1cff5bc0
Tuomas Alanen <tuomas.alanen@reaktor.com>
2022-12-29 15:41:03 +0200

    update deploy.sh to understand static site generation version

--- deploy.sh
+++ deploy.sh
@@ -6,34 +6,27 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/scripts" && pwd )"
 # shellcheck source=../../scripts/common-functions.sh
 source "$script_dir/common-functions.sh"
 
-dir="$repo/terraform"
+repo_dir="$repo/terraform"
+terraform_dir="$repo/terraform"
 
 function main {
-  pushd "$dir"
   require_command terraform
-  require_command docker
-  require_command git
+  use_correct_node_version
+
+  echo ::group::Build static site
+  pushd "$repo_dir"
+  npm ci
+  npm run build
+  npm run export
+  echo ::endgroup::
 
   echo ::group::Initialize Terraform
+  pushd "$terraform_dir"
   terraform init -input=false
   echo ::endgroup::
 
-  # Apply first only ECR repository
-  echo ::group::Apply ECR repository with Terraform
-  terraform apply -target=aws_ecr_repository.repository -input=false -auto-approve
-  export REPOSITORY_URL=$(terraform output --raw repository_url)
-  echo ::endgroup::
-  popd
-
-  echo ::group::Build and push Docker image
-  export TF_VAR_image_tag=$(git rev-parse --short HEAD)
-  docker build --platform linux/amd64 -t $REPOSITORY_URL:$TF_VAR_image_tag .
-  docker push $REPOSITORY_URL:$TF_VAR_image_tag
-  echo ::endgroup::
-
-  # Apply the rest of the infrastructure after the image is built and pushed
   echo ::group::Apply the rest of the infrastructure with Terraform
-  pushd "$dir"
+  pushd "$terraform_dir"
   terraform apply -input=false -auto-approve
   echo ::endgroup::
   }
--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.app.domain_name
-}
