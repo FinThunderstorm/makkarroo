286d26cc7bbedb6d7b2c27fa7abb5300f99f2292
Tuomas Alanen <tuomas.alanen@reaktor.com>
2022-12-30 13:40:25 +0200

    Improve styling to look even better

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,23 +1,28 @@
 import { Menu, Day, Food } from '../types'
 
 const MenuCard = ({ menu }: { menu: Menu }) => {
-    console.log(menu)
     return (
         <div key={menu.id} className="card bg-red-100">
-            <h2 className="card-header">{menu.restaurant}</h2>
+            <h2 className="card-header bg-gradient-to-r from-yellow-600 to-red-800 bg-clip-text font-mono text-3xl font-extrabold uppercase text-transparent">
+                {menu.restaurant}
+            </h2>
             <div className="card-body">
-                <p className="text-content2">{menu.address}</p>
+                <p className="mt-0 mb-2 font-mono lowercase">{menu.address}</p>
                 {menu.menu.map((item: Day) => (
-                    <div>
-                        <p>{item.date}</p>
-                        {item.items.map((food: Food) => (
-                            <p>
-                                {food.name}{' '}
-                                <span className="badge badge-outline-success badge-xs">
-                                    {food.price}
-                                </span>
-                            </p>
-                        ))}
+                    <div className="mt-2 mb-2">
+                        <p className="font-mono font-bold uppercase">
+                            {item.date}
+                        </p>
+                        <ul className="ml-8 list-disc">
+                            {item.items.map((food: Food) => (
+                                <li className="leading-relaxed">
+                                    {food.name}{' '}
+                                    <span className="badge badge-outline-success badge-xs">
+                                        {food.price}
+                                    </span>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 ))}
             </div>
