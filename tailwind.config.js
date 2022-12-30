ea0fec6948e951aef4f6e482b6cdb4f583840563
Tuomas Alanen <tuomas.alanen@reaktor.com>
2022-12-30 12:09:37 +0200

    run prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 /* eslint-disable global-require */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  plugins: [require("@tailwindcss/typography"), require("rippleui")],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}'
+    ],
+    plugins: [require('@tailwindcss/typography'), require('rippleui')]
 }
